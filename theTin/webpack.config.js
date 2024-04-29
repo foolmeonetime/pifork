299bb1fbb314851f2300f7a3d5e9540c06feb40c
foolmeonetime <166244674+foolmeonetime@users.noreply.github.com>
2024-04-28 20:35:57 -0700

    Add files via upload

---
+++ theTin/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // Other Webpack configuration settings...
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
+  };
+  
